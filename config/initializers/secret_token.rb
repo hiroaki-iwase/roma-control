2ad4359d0a20b434092b7ac09f991cfc5310baa8
hiroaki-iwase <hiroaki.iwase.r@gmail.com>
2014-01-21 16:10:51 +0900

    add Rails file

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RomaControl::Application.config.secret_token = 'ce7c247e0f31085b54f7bdf3b6d38ad0702f72e30efaf1ea0bd3a457b4320b91bf2921a223c8243d6023000c4af22877e62f39aa7883147ee1ce5e6b66f6594d'
