fcec7249f9da52206d90a56565fd993ceadbff7a
hiroaki-iwase <hiroaki.iwase.r@gmail.com>
2014-01-30 14:23:20 +0900

    enable change host & port

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,6 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 RomaControl::Application.initialize!
+
+# Include your application configuration below
+require 'config_gui'
---
+++ lib/config_gui.rb
@@ -0,0 +1,4 @@
+module ConfigGui
+  HOST = "10.0.2.15"
+  PORT = 10001
+end
