ac8489ff94232ee855492e311a0c7039ea13e93a
hiroaki-iwase <hiroaki.iwase.r@gmail.com>
2014-02-03 16:24:52 +0900

    making table

--- app/controllers/stat_controller.rb
+++ app/controllers/stat_controller.rb
@@ -12,7 +12,7 @@ class StatController < ApplicationController
     @others     = roma.stats_hash["dns_cashe"]
 =end
     #render :json => @stats_hash
-    #@stats_hash = roma.stats_hash
+    @stats_hash = roma.stats_hash
     @stats_json = roma.stats_json
   end
 
