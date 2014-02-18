8759cf170e66c1ab6426060e84f75ee886deecec
hiroaki-iwase <hiroaki.iwase.r@gmail.com>
2014-02-18 17:46:49 +0900

    add image & modify application.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,3 +13,18 @@
 //= require turbolinks
 //= require jquery-2.1.0
 //= require_tree .
+$(function(){
+
+    $("dd:not(:first)").css("display","none");
+    $("dl dt").click(function(){
+        if($("+dd",this).css("display")=="none"){
+            $("dd").slideUp("slow");
+            $("+dd",this).slideDown("slow");
+        }
+    });
+
+    $("[data-toggle=tooltip]").tooltip({
+      placement: 'left',
+      html: true
+    });
+});
