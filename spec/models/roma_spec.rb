6ba0249093dfd52f5e91a408dca88dbc93082a08
hiroaki-iwase <hiroaki.iwase.r@gmail.com>
2014-04-10 18:21:52 +0900

    add test of result of dynamic command

--- spec/models/roma_spec.rb
+++ spec/models/roma_spec.rb
@@ -1,6 +1,9 @@
 require 'spec_helper'
 
 describe Roma do
+
+#[Status check](ph1)=================================================================
+  #context "stats_result", :focus => true do
   context "stats_result" do
     res = Roma.new.stats
 
@@ -9,56 +12,93 @@ describe Roma do
       ## Config, Stats, Storage[roma], Write-behind, Routing, Connection, others
       expect(res.size).to be 7
     end
-
-    res.each{|k1, v1|
-      v1.each{|k2, v2|
-        it { expect(res[k1][k2]).not_to be_nil }
+    
+    #res["connection"]["debug"]= nil #debug
+    it "check all param have value(not have nil)" do
+      res.each{|k1, v1|
+        v1.each{|k2, v2|
+          expect(res[k1][k2]).not_to be_nil
+        }
       }
-    }
+    end
   end
 
-
+#[Dynamic command check](ph2)=================================================================
   share_examples_for 'dynamic cmd check' do |key, value, pattern|
-    let(:roma) { Roma.new(key => value) }
+    let(:roma) { Roma.new }
+    let(:dynamic) { roma.change_param(key, value) }
+    let(:actual_stats_normal) { roma.stats }
+
+    # return message check
+    it "Return param check[key=>#{key} / value = #{value} / check pattern=> Hash or not]" do
+      expect(dynamic).to be_true
+    end
+    it "Return param check[key=>#{key} / value = #{value} / check pattern=> Size > 1]" do
+      expect(dynamic.size).to be > 1
+    end
+    it "Return param check[key=>#{key} / value = #{value} / check pattern=> msg is 'STORED']" do
+      dynamic.values.each{|v| 
+        if key == "dns_caching"
+          expect(v.chomp).to eq("ENABLED")
+        else
+          expect(v.chomp).to eq("STORED")
+        end
+      }
+    end
+
+    # double array ToDO
+    case key
+    when "dcnice", "size_of_zredundant", "hilatency_warn_time", "spushv_klength_warn", "spushv_vlength_warn", "routing_trans_timeout"
+      hogehoge = "stats"
+    when "shift_size"
+      hogehoge = "write-behind"
+    when "fail_cnt_threshold", "fail_cnt_gap", "sub_nid", "lost_action", "auto_recover"
+      hogehoge = "routing"
+    when "dns_caching"
+      hogehoge = "others"
+    else
+      hogehoge = "connection"
+    end
+
     case pattern
-    when "normal"
-      it "normal test" do
-        expect(roma.valid?).to be_true
-      end
-    when "under0", "Over Limit", "Character", "nil", "Over Length", "Unexpected"
-      it "#{pattern} check" do
-        expect(roma.valid?).to be_false
-      end
+    when "digit"
+      #it { expect(actual_stats_normal[hogehoge][key].chomp ).to eq(value.to_s) }
+      it { expect(actual_stats_normal[hogehoge][key].chomp.to_i ).to eq(value) }
+
+    when "boolean"
+      it { expect(actual_stats_normal[hogehoge][key].chomp ).to eq(value) }
+      it { expect(actual_stats_normal[hogehoge][key].chomp ).to eq(value) }
     else
       raise
     end
   end
 
-  context "dynamic_command(normal)" do
-    roma = Roma.new
-    res_normal = roma.change_param("pool_expire_time", 777)
+  #context "dynamic_command(normal)", :focus => true do
+  #context "dynamic_command(normal)" do
+    #it_should_behave_like 'dynamic cmd check', "pool_expire_time", 777, "debug"
+  #context "dynamic_command(normal)" do
+  #  roma = Roma.new
+  #  res_normal = roma.change_param("pool_expire_time", 777)
 
-    it { expect(res_normal).to be_a_kind_of(Hash) }
-    it { expect(res_normal.size).to be > 1 } 
-    res_normal.values.each{|v| 
-      it { expect(v.chomp).to eq("STORED") }
-    }
+  #  it { expect(res_normal).to be_a_kind_of(Hash) }
+  #  it { expect(res_normal.size).to be > 1 } 
+  #  res_normal.values.each{|v| 
+  #    it { expect(v.chomp).to eq("STORED") }
+  #  }
 
-    actual_stats_normal = Roma.new.stats["connection"]["pool_expire_time"].chomp
-    it { expect(actual_stats_normal).to eq("777") }
-  end
+  #  actual_stats_normal = Roma.new.stats["connection"]["pool_expire_time"].chomp
+  #  it { expect(actual_stats_normal).to eq("777") }
+  #end
  
-  context "dynamic_command(in case of param error)" do
-    roma = Roma.new
-    it { expect { roma.change_param("EMpool_maxlength", -777) }.not_to raise_error}
-    #it { expect(roma.errors.full_messages[0]).to eq("Empool maxlength was not updated. Unexpection Error( CLIENT_ERRORlengthmustbegreaterthanzero\r\n ).") }
-    actual_stats_under0 = Roma.new.stats["connection"]["pool_expire_time"].chomp
-    it "confirm not changing" do 
-      expect(actual_stats_under0).to eq("777")
-    end
-  end
-
-
+  #context "dynamic_command(in case of param error)" do
+  #  roma = Roma.new
+  #  it { expect { roma.change_param("EMpool_maxlength", -777) }.not_to raise_error}
+  #  #it { expect(roma.errors.full_messages[0]).to eq("Empool maxlength was not updated. Unexpection Error( CLIENT_ERRORlengthmustbegreaterthanzero\r\n ).") }
+  #  actual_stats_under0 = Roma.new.stats["connection"]["pool_expire_time"].chomp
+  #  it "confirm not changing" do 
+  #    expect(actual_stats_under0).to eq("777")
+  #  end
+  #end
 
   share_examples_for 'validation check' do |key, value, pattern|
     let(:roma) { Roma.new(key => value) }
@@ -95,19 +135,23 @@ describe Roma do
   ]
 
   columns.each{|column|
-    context "dynamic_command_validation_normal" do 
+    context "dynamic_command_validation_normal", :focus => true do
+    #context "dynamic_command_validation_normal" do
+
       if column == "dns_caching" || column == "auto_recover"
+        it_should_behave_like 'dynamic cmd check', column, "true", "boolean"
         it_should_behave_like 'validation check', column, "true",  "normal"
         it_should_behave_like 'validation check', column, "false", "normal"
         it_should_behave_like 'validation check', column, "on", "Unexpected"
         it_should_behave_like 'validation check', column, "hogehoge", "Character"
-        it_should_behave_like 'validation check', column, nil, "nil"
+        #it_should_behave_like 'validation check', column, nil, "nil"
       else
+        it_should_behave_like 'dynamic cmd check', column, 40, "digit"
         it_should_behave_like 'validation check', column, 50, "normal"
         it_should_behave_like 'validation check', column, -50,        "under0"
         it_should_behave_like 'validation check', column, 9999999999, "Over Limit"
         it_should_behave_like 'validation check', column, "hogehoge", "Character"
-        it_should_behave_like 'validation check', column, nil,        "nil"
+        #it_should_behave_like 'validation check', column, nil,        "nil"
       end
     end
   }
@@ -162,5 +206,8 @@ describe Roma do
       end
     end
   }
+
+#[Cluster function check](ph3)=================================================================
+
 
 end # End of describe
