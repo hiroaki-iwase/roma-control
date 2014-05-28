1ab7d2b8224a9d2b3c1ad44eaaa185b7eefb95dc
hiroaki-iwase <hiroaki.iwase.r@gmail.com>
2014-05-28 16:12:33 +0900

    add http authentication check

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,17 @@
 class ApplicationController < ActionController::Base
+  before_filter :auth
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  private
+  def auth
+    name = 'roma'
+    passwd = '8cb2237d0679ca88db6464eac60da96345513964' #12345
+    authenticate_or_request_with_http_basic('Gladiator') do |n, p|
+      n == name &&
+        Digest::SHA1.hexdigest(p) == passwd
+    end
+  end
+
 end
