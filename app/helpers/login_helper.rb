badc04780a59293bdabac6a8b1430c41c1a5dedc
hiroaki-iwase <hiroaki.iwase.r@gmail.com>
2014-06-06 13:53:25 +0900

    change css indent & gravatar setting & other designs

--- app/helpers/login_helper.rb
+++ app/helpers/login_helper.rb
@@ -2,7 +2,7 @@ module LoginHelper
 
   def get_gravatar_src(email_address = nil)
     hash = Digest::MD5.hexdigest(email_address)
-    image_src = "http://www.gravatar.com/avatar/#{hash}"
+    image_src = "http://www.gravatar.com/avatar/#{hash}?d=mm"
   end
 
 end
