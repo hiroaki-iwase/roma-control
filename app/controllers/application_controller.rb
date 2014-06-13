5933d92749bee5a7f7f67cd6c3b15b31413199d1
hiroaki-iwase <hiroaki.iwase.r@gmail.com>
2014-06-13 16:35:39 +0900

    add controller test(api)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,12 @@
 class ApplicationController < ActionController::Base
-  before_filter :check_logined
+  before_filter :check_logined_filter
+  helper_method :login_check?
 
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  private
   def login_check?
     if session[:username] && session[:password] && User.authenticate(session[:username], session[:password])
       return true
@@ -12,15 +14,12 @@ class ApplicationController < ActionController::Base
     false
   end
 
-  private
-  def check_logined
+  def check_logined_filter
     flash[:referer] = request.fullpath
 
     if session[:username] && session[:password]
       begin
         raise if !User.authenticate(session[:username], session[:password])
-        @authorization = 'root'
-        #@authorization = 'normal' #toDO
       rescue
         reset_session
         flash[:filter_msg] = "illegal user data!!"
