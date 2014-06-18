80381e54786e67ad6c6c9c188dc91915f1cc4024
hiroaki-iwase <hiroaki.iwase.r@gmail.com>
2014-06-18 18:03:43 +0900

    finish to develop phase4

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -3,7 +3,7 @@
 module Constants
   private
   # version
-  VERSION = "0.4.8"
+  VERSION = "0.5.0"
   # stats colomn name
   STATS_COL_NAME = ["config","stats","storages[roma]","write-behind","routing","connection","others"]
 
