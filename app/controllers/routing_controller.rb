6a8b8ceee593e847adf25aa3290efa9d83b253c5
hiroaki-iwase <hiroaki.iwase.r@gmail.com>
2014-06-30 11:37:31 +0900

    developed routing option column

--- app/controllers/routing_controller.rb
+++ app/controllers/routing_controller.rb
@@ -8,7 +8,8 @@ class RoutingController < ApplicationController
       @active_routing_list = roma.change_roma_res_style(@stats_hash["routing"]["nodes"])
       #@inactive_routing_list = roma.get_all_routing_list - @active_routing_list
       #@routing_info = roma.get_routing_info(@active_routing_list)
- 
+      @enabled_repetition_in_routingdump = roma.enabled_repetition_in_routingdump?
+
     rescue => @ex
       render :template => "errors/error_500", :status => 500
     end
