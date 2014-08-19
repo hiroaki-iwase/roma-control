b5809ea8305e22c5402a1a26fec4468e24556f6c
hiroaki-iwase <hiroaki.iwase.r@gmail.com>
2014-08-19 02:10:10 +0000

    handle in controller

--- app/assets/javascripts/login.js
+++ app/assets/javascripts/login.js
@@ -10,95 +10,13 @@ $(function(){
         }
     }
 
-//    //version check
-//    $("#loginButton").click(function() {
-//        version = checkRomaVersion();
-//        if (version >= 65536) { // over v1.0.0
-//           //access to auth
-//            var userName = $('#userNameValue').val();
-//            var passWord = $('#passWordValue').val();
-//            login(userName, passWord);
-//
-//        } else if ( version >= 2059 && version < 65536 ) { //v0.8.11 - v0.8.14
-//            var userName = $('#userNameValue').val();
-//            var passWord = $('#passWordValue').val();
-//            $('#loginUserName').val(userName);
-//            $('#loginPassword').val(passWord);
-//            $('#version-modal').modal({
-//                show: true,
-//                keyboard: false,
-//            });
-//        } else { // below v0.8.10 and unexpected version
-//            $('.password').val('');
-//            $('#version-restrict-modal').modal({
-//                show: true,
-//                keyboard: false,
-//            });
-//            alert('no s');
-//        }
-//    });
-//
-//    function checkRomaVersion() {
-//    //      $('#plugin-modal').modal({
-//    //          show: true,
-//    //      });
-//        return 777;
-//    //    //var host = instance.split('_')[0]
-//    //    //var port = instance.split('_')[1]
-//
-//    //    var webApiEndpoint = setApiEndpoint('get_parameter')
-//
-//    //    alert(webApiEndpoint);
-//
-//    //    $.ajax({
-//    //        //url: webApiEndpoint+"/"+host+"/"+port,
-//    //        url: webApiEndpoint,
-//    //        type: 'GET',
-//    //        dataType: 'json',
-//    //        cache: false,
-//    //    }).done(function(data){
-//    //    alert('sss');
-//    //    }).fail(function(){
-//    //      alert("fail to check ROMA version");
-//    //    });
-//    //}//End of checkRomaVersion
-//
-//    //function setApiEndpoint(action) {
-//    //    var protocol = location.protocol;
-//    //    var host = location.host;
-//    //    var webApiEndpoint = protocol+"//"+host+"/api/"+action;
-//    //    return webApiEndpoint;
-//    }
-//
-//    //function login(userName, passWord) {
-//    ////    //var host = instance.split('_')[0]
-//    ////    //var port = instance.split('_')[1]
-//
-//    //var webApiEndpoint = 'http://192.168.223.2:3000/login/auth'
-//
-//    ////    var webApiEndpoint = setApiEndpoint('get_parameter')
-//
-//    //var data = {username:userName, password:passWord}
-//    //    $.ajax({
-//    //        url: webApiEndpoint,
-//    //        type: 'POST',
-//    //        data: {
-//    //            username: userName,
-//    //            password: passWord
-//    //        },
-//    //        dataType: "html",
-//    //        cache: false,
-//    //    }).done(function(data){
-//    //        console.log('success to login');
-//    //    }).fail(function(){
-//    //        alert("fail to check ROMA version");
-//    //    });
-//
-//    ////function setApiEndpoint(action) {
-//    ////    var protocol = location.protocol;
-//    ////    var host = location.host;
-//    ////    var webApiEndpoint = protocol+"//"+host+"/api/"+action;
-//    ////    return webApiEndpoint;
-//    //}
-
+    //version check
+    if($(".login-error").length){
+        errorMsg = $(".login-error").text();
+        if (errorMsg.match(/unsupport version/)) {
+          $('#version-restrict-modal').modal({
+              show: true,
+          });
+        }
+    }
 });
--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -20,9 +20,16 @@ class LoginController < ApplicationController
       ###Version check(ROMA main unit)
       version = self.class.helpers.chk_roma_version(stats_hash['others']['version'])
       case version
+      when 0..2058 # earlier than v0.8.10 
+        reset_session
+        flash[:login_error] = 'unsupport version'
+        redirect_to :action => 'index' and return
+      when 2059..65535 # v0.8.11 - v0.8.14
+        reset_session
+        flash[:login_error] = 'unsupport version'
+        redirect_to :action => 'index' and return
       when 65536..Float::INFINITY # over 1.0.0
-      else
-        session[:unsupport] = true
+        # nothing
       end
 
       ###plugin check(config_gui.rb)
